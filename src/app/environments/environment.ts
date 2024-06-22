bcb096dd3d2d200244045a3a1dbc061e0a6cd271
Chippo156 <vovannghiahiep150623@gmail.com>
2024-06-22 17:48:36 +0700

    update http

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
-export const environtment =  {
-  production : false,
-  apiBaseUrl : 'http://localhost:8080/api/v1'
-}
+export const environtment = {
+  production: false,
+  // apiBaseUrl : 'http://localhost:8080/api/v1'
+  apiBaseUrl: 'https://frontend-app-cvcz.onrender.com/api/v1',
+};
